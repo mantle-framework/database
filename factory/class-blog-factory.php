db914e94bd0624dd7018c424814fbe3cb506bc9c
mantle-ci <mantle@alley.com>
2025-09-16 12:47:40 +0000

    Fix payload of slack message

--- factory/class-blog-factory.php
+++ factory/class-blog-factory.php
@@ -8,6 +8,7 @@
 namespace Mantle\Database\Factory;
 
 use Mantle\Database\Model\Site;
+use WP_Network;
 
 use function Mantle\Support\Helpers\get_site_object;
 
@@ -36,12 +37,40 @@ class Blog_Factory extends Factory {
 	public function definition(): array {
 		global $current_site, $base;
 
+		assert( $current_site instanceof WP_Network, 'Expected $current_site to be an instance of WP_Network' );
+
 		return [
 			'domain'     => $current_site->domain,
-			'path'       => $base . $this->faker->slug(),
-			'title'      => $this->faker->text(),
+			'path'       => $base . $this->faker->unique()->slug(),
+			'title'      => $this->faker->unique()->text( 20 ),
 			'network_id' => $current_site->id,
 		];
+	}
+
+	/**
+	 * Create a subdomain of the current site.
+	 */
+	public function subdomain(): static {
+		global $current_site;
+
+		assert( $current_site instanceof WP_Network, 'Expected $current_site to be an instance of WP_Network' );
+
+		return $this->state( [
+			'domain' => $this->faker->unique()->domainWord() . '.' . $current_site->domain,
+			'path'   => '/',
+		] );
+	}
+
+	/**
+	 * Create a site with the given domain.
+	 *
+	 * @param string $domain The domain to use.
+	 */
+	public function domain( string $domain ): static {
+		return $this->state( [
+			'domain' => $domain,
+			'path'   => '/',
+		] );
 	}
 
 	/**
